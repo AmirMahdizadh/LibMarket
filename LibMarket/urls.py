cd3101a4cbb69297aed50f4ff4052987d9fa2d14
Amir <amirmahdizadh143@gmail.com>
2024-08-19 11:48:17 +0330

    change the static folder

--- LibMarket/urls.py
+++ LibMarket/urls.py
@@ -1,4 +1,4 @@
-"""eshop_project URL Configuration
+""" LibMarket URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
