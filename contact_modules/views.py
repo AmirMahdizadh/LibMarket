14e8513b13d0e31295499fb1305fc12ddf517313
Amir <amirmahdizadh143@gmail.com>
2024-09-14 13:12:11 +0330

    67

--- contact_modules/views.py
+++ contact_modules/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
 # Create your views here.
+from django.urls import reverse
 
 def contact_us_page(request):
+    if request.method == 'POST':
+        print(request.POST['email'])
+        print(request.POST['fullname'])
+        print(request.POST['subject'])
+        print(request.POST['message'])
+        return redirect(reverse('home_page'))
     return render(request, 'contact_modules/contact_us_page.html')
